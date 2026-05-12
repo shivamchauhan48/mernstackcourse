9ea21070b0b99cb62b8a7044ecd4d7c4ca2887de
Shivam Chauhan <shivamchauhan48@gmail.com>
2026-05-12 09:20:29 +0530

    Task 16 completed learned the flag method...

---
+++ TASK16/script.js
@@ -0,0 +1,106 @@
+let data = [
+  { name: "karan", marks: 80, class: "9th", address: "delhi" },
+  { name: "sameer", marks: 50, class: "10th", address: "noida" },
+  { name: "rony", marks: 90, class: "9th", address: "meerut" },
+];
+
+let main = document.getElementById("main");
+
+let search = document.getElementById("search");
+
+let input = document.createElement("input");
+search.appendChild(input);
+
+let inputbtn = document.createElement("button");
+
+inputbtn.innerText = "Click";
+search.appendChild(inputbtn);
+let searchAddress;
+inputbtn.addEventListener("click", () => {
+  // console.log("Clicked", input.value, "--");
+  searchAddress = input.value;
+  displayFunction();
+});
+/*
+if (searchAddress) {
+  data.map((element) => {
+    if (toLowerCase(element.address) == toLowerCase(searchAddress)) {
+      console.log(element);
+      console.log(element.name);
+      console.log(element.class);
+      let newnode = document.createElement("div");
+      newnode.innerHTML = `<div class="card">
+     <h2>${element.name}</h2>
+     <h3>Class:${element.class}</h3>
+     <h3>Marks:${element.marks}</h3>
+     <h3>Address:${element.address}</h3>
+     </div>
+     `;
+      main.appendChild(newnode);
+    } else {
+      console.log("No match found");
+      main.innerHTML = "<div>No Match Found</div>";
+    }
+  });
+} else {
+  data.map((element) => {
+    console.log(element);
+    console.log(element.name);
+    console.log(element.class);
+    let newnode = document.createElement("div");
+    newnode.innerHTML = `<div class="card">
+    <h2>${element.name}</h2>
+    <h3>Class:${element.class}</h3>
+    <h3>Marks:${element.marks}</h3>
+    <h3>Address:${element.address}</h3>
+    </div>
+    `;
+
+    main.appendChild(newnode);
+  });
+}
+*/
+
+const displayFunction = () => {
+  if (searchAddress == "") {
+    data.map((element) => {
+      // console.log(element);
+      // console.log(element.name);
+      // console.log(element.class);
+      let newnode = document.createElement("div");
+      newnode.innerHTML = `<div class="card">
+      <h2>${element.name}</h2>
+      <h3>Class:${element.class}</h3>
+      <h3>Marks:${element.marks}</h3>
+      <h3>Address:${element.address}</h3>
+      </div>
+      `;
+
+      main.appendChild(newnode);
+    });
+  } else {
+    let flag = false;
+    main.innerHTML = "";
+    data.map((element) => {
+      console.log(element.address, searchAddress);
+      if (element.address == searchAddress) {
+        flag = true;
+        console.log(element);
+        console.log(element.name);
+        console.log(element.class);
+        let newnode = document.createElement("div");
+        newnode.innerHTML = `<div class="card">
+        <h2>${element.name}</h2>
+        <h3>Class:${element.class}</h3>
+        <h3>Marks:${element.marks}</h3>
+        <h3>Address:${element.address}</h3>
+        </div>
+        `;
+        main.appendChild(newnode);
+      }
+      if (!flag) {
+        console.log("No match found....");
+      }
+    });
+  }
+};
